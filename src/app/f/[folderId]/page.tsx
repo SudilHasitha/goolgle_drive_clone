69ff375af071f08f78d19d8d24f84302e53e9b48
SudilHasitha <sudilhasithaa51@gmail.com>
2025-03-01 20:32:20 +0530

    fix: tidy up move data fetching to server

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,8 +1,26 @@
 import { files as filesSchema, folders as foldersSchema } from "~/server/db/schema";
 import { db } from "~/server/db";
 import DriveContents from "../../drive-contents";
-import { z } from "zod";
 import { eq } from "drizzle-orm";
+
+async function getAllParent(folderId: number) {
+    const parents = []
+    let currentId: number | null = folderId;
+    while (currentId !== null) {
+        const folder = await db
+            .selectDistinct()
+            .from(foldersSchema)
+            .where(eq(foldersSchema.id, currentId));
+
+        if (!folder[0]) {
+            throw new Error("Folder not found");
+        }
+
+        parents.unshift(folder[0]);
+        currentId = folder[0]?.parent;
+    }
+    return parents;
+}
 
 export default async function GoogleDriveClone(props: { params: Promise<{ folderId: string }> }) {
 
@@ -15,15 +33,25 @@ export default async function GoogleDriveClone(props: { params: Promise<{ folder
 
     console.log("Folder ID: ", params.folderId);
 
-    const files = await db
+    const filesPromise = db
             .select()
             .from(filesSchema)
             .where(eq(filesSchema.parent, parsedFolderId));
-    const folders = await db
+    const foldersPromise = db
             .select()
             .from(foldersSchema)
             .where(eq(foldersSchema.parent, parsedFolderId));
+    
+    const parentsPromise = getAllParent(parsedFolderId);
 
-    return <DriveContents files={files} folders={folders} />;
+    /**
+     * Hi Monkey think about error hangling when a folder structure get altered
+     * such as intermediate folder is deleted
+     * parentsPromise will throw an error
+     * 
+     * So think about how to handle this error
+     */
+    const [folders, files, parents] = await Promise.all([foldersPromise, filesPromise, parentsPromise]);
+    return <DriveContents files={files} folders={folders} parents={parents} />;
 
 }
